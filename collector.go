4cdfb19f5265d1e4b7fbe8021ee80c275b15a175
bongwoo <bongwoo_bak@tmax.co.kr>
2021-03-30 16:19:27 +0900

    [add] collector.go

---
+++ collector.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+//Define a struct for you collector that contains pointers
+//to prometheus descriptors for each metric you wish to expose.
+//Note you can also include fields of other types if they provide utility
+//but we just won't be exposing them as metrics.
+type k8sCollector struct {
+	k8sMetric *prometheus.Desc
+	barMetric *prometheus.Desc
+}
+
+//You must create a constructor for you collector that
+//initializes every descriptor and returns a pointer to the collector
+func newK8sCollector() *k8sCollector {
+	return &k8sCollector{
+		k8sMetric: prometheus.NewDesc("k8s_metric",
+			"test metric",
+			nil, nil,
+		),
+		barMetric: prometheus.NewDesc("bar_metric",
+			"test_metric2",
+			nil, nil,
+		),
+	}
+}
+
+//Each and every collector must implement the Describe function.
+//It essentially writes all descriptors to the prometheus desc channel.
+func (collector *k8sCollector) Describe(ch chan<- *prometheus.Desc) {
+
+	//Update this section with the each metric you create for a given collector
+	ch <- collector.k8sMetric
+	ch <- collector.barMetric
+}
+
+//Collect implements required collect function for all promehteus collectors
+func (collector *k8sCollector) Collect(ch chan<- prometheus.Metric) {
+
+	//Implement logic here to determine proper metric value to return to prometheus
+	//for each descriptor or call other functions that do so.
+	var metricValue float64
+	if 1 == 1 {
+		metricValue = 1
+	}
+
+	//Write latest value for each metric in the prometheus metric channel.
+	//Note that you can pass CounterValue, GaugeValue, or UntypedValue types here.
+	ch <- prometheus.MustNewConstMetric(collector.k8sMetric, prometheus.CounterValue, metricValue)
+	ch <- prometheus.MustNewConstMetric(collector.barMetric, prometheus.CounterValue, metricValue)
+
+}
--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"net/http"
 
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	log "github.com/sirupsen/logrus"
 )
 
 func main() {
-
+	k8s := newK8sCollector()
+	prometheus.MustRegister(k8s)
 	http.Handle("/metrics", promhttp.Handler())
 	log.Info("Beginning to serve on port :8080")
 }
