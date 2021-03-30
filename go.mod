fa8cdac0afe26566b85853a19499f8f76641f890
bongwoo <bongwoo_bak@tmax.co.kr>
2021-03-30 15:49:13 +0900

    go.mod

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/tmax-cloud/k8s-exporter
+
+go 1.15
+
+require github.com/prometheus/client_golang v1.10.0 // indirect
--- main.go
+++ main.go
@@ -4,9 +4,11 @@ import (
 	"net/http"
 
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
 
 	http.Handle("/metrics", promhttp.Handler())
+	log.Info("Beginning to serve on port :8080")
 }
